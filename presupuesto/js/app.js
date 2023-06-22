478c3b1596f4851de5d245462162ca45d7e20294
Flavio Scheck <flavio.scheck@gmail.com>
2023-06-22 16:31:59 -0300

    add presupuesto-app

---
+++ presupuesto/js/app.js
@@ -0,0 +1,130 @@
+const ingresos = [
+    new Ingreso('Salario', 2100),
+    new Ingreso('Venta de coche', 1500)
+];
+
+const egresos = [
+    new Egreso('Pago de alquiler', 800),
+    new Egreso('Ropa', 400)
+];
+
+let cargarApp = () => {
+    cargarCabecero();
+    cargarIngresos();
+    cargarEgresos();
+}
+
+let totalIngresos = () => {
+    let totalIngreso = 0;
+    for(let ingreso of ingresos){
+        totalIngreso += ingreso.valor;
+    }
+    return totalIngreso;
+}
+
+let totalEgresos = () => {
+    let totalEgreso = 0;
+    for(let egreso of egresos){
+        totalEgreso += egreso.valor;
+    }
+    return totalEgreso;
+}
+
+let cargarCabecero = () => {
+    let presupuesto = totalIngresos() - totalEgresos();
+    let porcentajeEgresos = totalEgresos()/totalIngresos();
+    document.getElementById('presupuesto').innerHTML = formatoMoneda(presupuesto);
+    document.getElementById('porcentaje').innerHTML = formatoPorcentaje(porcentajeEgresos);
+    document.getElementById('ingresos').innerHTML = formatoMoneda(totalIngresos());
+    document.getElementById('egresos').innerHTML = formatoMoneda(totalEgresos());
+}
+
+const formatoMoneda = (valor)=>{
+    return valor.toLocaleString('es-AR', {style:'currency', currency:'ARS', minimumFractionDigits:2});
+}
+
+const formatoPorcentaje = (valor)=>{
+    return valor.toLocaleString('es-AR', {style:'percent', minimumFractionDigits:2});
+}
+
+const cargarIngresos = () =>{
+    let ingresosHtml = '';
+    for(let ingreso of ingresos){
+        ingresosHtml += crearIngresoHtml(ingreso);
+    }
+    document.getElementById('lista-ingresos').innerHTML = ingresosHtml;
+}
+
+const crearIngresoHtml = (ingreso) => {
+    let ingresoHtml = ` <div class="elemento limpiarEstilos">
+    <div class="elemento_descripcion">${ingreso.descripcion}</div>
+    <div class="derecha limpiarEstilos">
+        <div class="elemento_valor">+ ${formatoMoneda(ingreso.valor)}</div>
+        <div class="elemento_eliminar">
+            <button class="elemento_eliminar--btn">
+                <ion-icon name="close-circle-outline"
+                onclick="eliminarIngreso(${ingreso.id})"></ion-icon>
+            </button>
+        </div>
+    </div>
+</div>`;
+    return ingresoHtml;
+}
+
+const eliminarIngreso = (id) => {
+    let indiceEliminar = ingresos.findIndex(ingreso=> ingreso.id === id);
+    ingresos.splice(indiceEliminar, 1);
+    cargarCabecero();
+    cargarIngresos();
+}
+
+const cargarEgresos = () => {
+    let egresosHtml = '';
+    for(let egreso of egresos){
+        egresosHtml += crearEgresoHtml(egreso);
+    }
+    document.getElementById('lista-egresos').innerHTML = egresosHtml;
+}
+
+const crearEgresoHtml = (egreso) => {
+    let egresoHtml = `<div class="elemento limpiarEstilos">
+    <div class="elemento_descripcion">${egreso.descripcion}</div>
+    <div class="derecha limpiarEstilos">
+        <div class="elemento_valor">- ${formatoMoneda(egreso.valor)}</div>
+        <div class="elemento_porcentaje">${formatoPorcentaje(egreso.valor/totalEgresos())}</div>
+        <div class="elemento_eliminar">
+            <button class="elemento_eliminar--btn">
+                <ion-icon name="close-circle-outline"
+                onclick="eliminarEgreso(${egreso.id})"></ion-icon>
+            </button>
+        </div>
+    </div>
+</div>`;
+    return egresoHtml;
+}
+
+const eliminarEgreso = (id) => {
+    let indiceEliminar = egresos.findIndex(egreso=> egreso.id === id);
+    egresos.splice(indiceEliminar, 1);
+    cargarCabecero();
+    cargarEgresos();
+}
+
+let agregarDato = () => {
+    let forma = document.forms['forma'];
+    let tipo = forma['tipo'];
+    let descripcion = forma['descripcion'];
+    let valor = forma['valor'];
+    if(descripcion.value !== '' && valor.value !== ''){
+        if(tipo.value === 'ingreso'){
+            ingresos.push(new Ingreso(descripcion.value, Number(valor.value)));
+            cargarCabecero();
+            cargarIngresos();
+        }
+        else if(tipo.value === 'egreso'){
+            egresos.push(new Egreso(descripcion.value, +valor.value));
+            cargarCabecero();
+            cargarEgresos();
+        }
+    }
+}
